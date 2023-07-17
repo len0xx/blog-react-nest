e7b2db1d3561752d87d6e4b048775f8d2867e85a
Prokhor Minin <len0xx@icloud.com>
2023-07-17 15:19:54 +0500

    Minor fixes

--- frontend/components/TipTap.tsx
+++ frontend/components/TipTap.tsx
@@ -67,19 +67,12 @@ const TipTap = forwardRef<Editor, {}>((_props, ref: Ref<Editor>) => {
         const { doc, selection } = state
         const { empty } = selection
 
-        // Sometime check for `empty` is not enough.
-        // Doubleclick an empty paragraph returns a node size of 2.
-        // So we check also for an empty text size.
         const isEmptyTextBlock = !doc.textBetween(from, to).length && isTextSelection(state.selection)
-
-        // When clicking on a element inside the bubble menu the editor "blur" event
-        // is called and the bubble menu item is focussed. In this case we should
-        // consider the menu as part of the editor and keep showing the menu
         const isChildOfMenu = menuWrapper.current!.contains(document.activeElement)
-
         const hasEditorFocus = view.hasFocus() || isChildOfMenu
+        const blocksActive = editor!.isActive('image') || editor!.isActive('codeBlock') || editor!.isActive('heading')
 
-        if (!hasEditorFocus || empty || isEmptyTextBlock || !editor!.isEditable || editor?.isActive('image')) {
+        if (!hasEditorFocus || empty || isEmptyTextBlock || !editor!.isEditable || blocksActive) {
             return false
         }
 
