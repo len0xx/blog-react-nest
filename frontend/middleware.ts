df352ef222ecbea7780cbc52d138b85307829e8b
Prokhor Minin <len0xx@icloud.com>
2023-07-19 14:09:59 +0500

    Redirect unauthorized users from /profile to /login

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -3,13 +3,14 @@ import type { NextRequest } from 'next/server'
 import { getToken } from 'next-auth/jwt'
 
 export async function middleware(request: NextRequest) {
+    const PROTECTED_ROUTES = [ '/new', '/profile' ]
     const token = await getToken({
         req: request,
         secret: process.env.NEXTAUTH_SECRET
     })
     const authenticated = !!(token && token.id && token.email)
 
-    if (!authenticated && request.nextUrl.pathname === '/new') {
+    if (!authenticated && PROTECTED_ROUTES.includes(request.nextUrl.pathname)) {
         return NextResponse.redirect(new URL('/login', request.url))
     }
 }
