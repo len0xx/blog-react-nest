b9071986c2df4ab92438dc0c1f2a0a432d19cfe5
Prokhor Minin <len0xx@icloud.com>
2023-07-22 09:57:51 +0500

    Create a 404 error page

---
+++ frontend/app/not-found.tsx
@@ -0,0 +1,12 @@
+export default function NotFound() {
+    return (
+        <div className="not-found-page">
+            <title>404 Not Found</title>
+            <h2>Not Found</h2>
+            <p>Could not find requested resource</p>
+            <p>
+                <a href="/">Go to the main page</a>
+            </p>
+        </div>
+    )
+}
