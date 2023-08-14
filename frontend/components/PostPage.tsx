c8ae63e37d6eb4a11fd3279f3b9fcbdeff03042e
Prokhor Minin <len0xx@icloud.com>
2023-08-14 13:47:02 +0500

    Remove Go back button from post page

--- frontend/components/PostPage.tsx
+++ frontend/components/PostPage.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import Button from './Button'
 import Content from './Content'
 
 interface ComponentProps {
@@ -8,14 +7,11 @@ interface ComponentProps {
 	content: string
 }
 
-const goBack = () => window.history.back()
-
 export default function PostPage({ title, content }: ComponentProps) {
 	return (
 		<div className='post-page-content' data-testid="post-content">
 			<h1>{ title }</h1><br />
 			<Content content={ content } /><br />
-			<Button onClick={ goBack }>Go back</Button>
 		</div>
 	)
 }
